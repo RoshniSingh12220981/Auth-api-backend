949cbe6c7a3f6d3c98e565d6bf4cb7393aa2ff84
RoshniSingh12220981 <roshnisinghranjan26@gmail.com>
2025-06-05 00:29:10 +0530

    Add root route to server.js to show server running message

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = require('./app');
 // Load environment variables
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running 🚀');
+});
+
 // Start the server
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
